eda02c80f83c953cd6aee9753e7b7fe66c7951b8
Caio Felipe <75501510+caiovfelipe@users.noreply.github.com>
2023-01-26 16:16:11 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('#form-habits')
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
+
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0, 5)
+  const dayExists = nlwSetup.dayExists(today)
+  if (dayExists) {
+    alert('Dia já incluso🛑')
+    return
+  }
+
+  alert('Adicionado com sucesso✅')
+  nlwSetup.addDay(today)
+}
+
+function save() {
+
+  localStorage.setItem('Nlwdata@habits', JSON.stringify(nlwSetup.data))
+  console.log(nlwSetup.data)
+}
+const data = JSON.parse(localStorage.getItem("Nlwdata@habits")) || {}
+
+
+nlwSetup.setData(data)
+nlwSetup.load() 
+
